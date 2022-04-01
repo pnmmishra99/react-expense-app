db114736a4c15bc30b67d3297850350bb84c25b8
poonam mishra <pnmmishra99@gmail.com>
2022-04-01 15:24:22 +0530

    added card and expenses component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,15 +1,16 @@
 import "./ExpenseDate.css";
+import Card from "./Card";
 
 function ExpenseDate(props) {
   const month = props.date.toLocaleDateString("en-US", { month: "long" });
   const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
-    </div>
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </Card>
   );
 }
 
---
+++ src/components/Expenses.js
@@ -0,0 +1,27 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amount={props.item[0].amount}
+        date={props.item[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[1].title}
+        amount={props.item[1].amount}
+        date={props.item[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[2].title}
+        amount={props.item[2].amount}
+        date={props.item[2].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
